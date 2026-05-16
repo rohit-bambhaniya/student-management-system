6a06ed35a769e4a813b21e2efebbb002d5ec3d3a
rohit.bambhaniya <rohit.bambhaniya.dev@gmail.com>
2026-05-16 21:22:57 +0530

    Edit colors in login.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,21 +7,10 @@ urlpatterns = [
 
     path('logout/', views.logout_view, name='logout'),
 
-    path(
-        'admin-dashboard/',
-        views.admin_dashboard,
-        name='admin_dashboard'
-    ),
+    path('admin-dashboard/',views.admin_dashboard,name='admin_dashboard'),
 
-    path(
-        'teacher-dashboard/',
-        views.teacher_dashboard,
-        name='teacher_dashboard'
-    ),
+    path('teacher-dashboard/',views.teacher_dashboard,name='teacher_dashboard'),
 
-    path(
-        'student-dashboard/',
-        views.student_dashboard,
-        name='student_dashboard'
-    ),
+    path('student-dashboard/',views.student_dashboard,name='student_dashboard'),
+    
 ]
